4cbdc6df19cf550af4609c2c9b5c012f00dc5cff
Jeter Chen <dali1756@gmail.com>
2024-06-28 01:35:52 +0800

    remove tailwind CDN

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-  "./src/*.{html,js}",
+    "./templates/*.{html,js}",
+    "./src/*.{html,js}",
   ],
   theme: {
     extend: {},
